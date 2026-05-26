c93cf3b9fc9b4666f4a70e34a0d8024d59f2ee4b
YannMassoumou <YannPC>
2026-05-27 00:25:50 +0200

    Added LastName to the database and change the students class, but I have issue on the API still populating same name, need investigation

--- Student.cs
+++ Student.cs
@@ -3,10 +3,10 @@
     public class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "";
-
-        public string LastName { get; set; } = "";
+        public string FirstName { get; set; } = "";
         public int Age { get; set; }
         public string Grade { get; set; } = "";
+
+        public string LastName { get; set; } = "";
     }
 }
